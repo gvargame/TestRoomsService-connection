1b9c1dabc001cf9ebf023299c447fc196dcb8b1b
pro100kryto <pro100kryto.original@gmail.com>
2021-11-15 11:50:58 -0300

    Create empty ITestRoomsServiceConnection

---
+++ src/main/java/com/gvargame/server/services/testrooms/connection/ITestRoomsServiceConnection.java
@@ -0,0 +1,6 @@
+package com.gvargame.server.services.testrooms.connection;
+
+import com.pro100kryto.server.service.IServiceConnection;
+
+public interface ITestRoomsServiceConnection extends IServiceConnection {
+}
